0e6ab09ee2b33ea4e59796acc547010e99a0f060
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-20 16:18:26 -0700

    Add create action to lists controller. Add list_params method to accommodate rails 4 strong params requirements.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,18 @@
 class ListsController < ApplicationController
   def index
   end
+
+  def create
+    list = List.create(list_params)
+    if list.save
+      render json: list
+    else
+      render json: { errors: list.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title)
+  end
 end
