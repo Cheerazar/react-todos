5b85027f260195f515db6a2fbfb66f42d6894356
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-20 14:37:49 -0700

    Remove create_todo_lists migration. Add create_lists migration. Update schema with after running new migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920205418) do
+ActiveRecord::Schema.define(version: 20140920211950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "todo_lists", force: true do |t|
+  create_table "lists", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
