8750534c26809528042fcc5899318807e621bb61
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-20 21:12:12 -0700

    Add update action to lists controller.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,6 +17,16 @@ class ListsController < ApplicationController
     render json: { id: params[:id].to_i }
   end
 
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    if list.save
+      render json: list
+    else
+      render json: { errors: list.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
   def list_params
     params.require(:list).permit(:title)
