a0908fe59ccde2b02ed5004782e619722abeee76
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-20 19:44:37 -0700

    Add destroy action to lists controller.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,6 +12,11 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    List.destroy(params[:id])
+    render json: { id: params[:id].to_i }
+  end
+
   private
   def list_params
     params.require(:list).permit(:title)
