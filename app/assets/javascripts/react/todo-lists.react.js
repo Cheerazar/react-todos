38e0f32eff18227a3fb6c83cbb0834c26b07d592
Gunnari Auvinen <gauvinen@gmail.com>
2014-09-20 18:30:18 -0700

    Update todo-lists component.   - Require ListStore   - add getInitialState to get todoLists   - add componentDidMount function to listen for 'change' trigger, and update state based on that.   - Update render method to deal with new implementation of state.

--- app/assets/javascripts/react/todo-lists.react.js
+++ app/assets/javascripts/react/todo-lists.react.js
@@ -2,14 +2,33 @@
 //= require react.min
 //= require react/todo-list.react
 //= require react/todo-list-create.react
+//= require stores/list-store
 
 var TodoLists = React.createClass({displayName: 'TodoLists',
+  getInitialState: function() {
+    return {
+      todoLists: ListStore.lists()
+    };
+  },
+
+  componentDidMount: function() {
+    $(ListStore).on('change', function () {
+      this.setState({
+        todoLists: ListStore.lists()
+      });
+    }.bind(this));
+    ListStore.getLists();
+  },
+
   render: function () {
+    var todoLists = [];
+    this.state.todoLists.forEach(function (todo) {
+      todoLists.push(TodoList({todo: todo}));
+    });
     return (
       React.DOM.div(null, 
         TodoListCreate(null), 
-        TodoList(null)
-
+        todoLists
       )
     );
   }
--- app/assets/react/todo-lists.react.js
+++ app/assets/react/todo-lists.react.js
@@ -2,14 +2,33 @@
 //= require react.min
 //= require react/todo-list.react
 //= require react/todo-list-create.react
+//= require stores/list-store
 
 var TodoLists = React.createClass({
+  getInitialState: function() {
+    return {
+      todoLists: ListStore.lists()
+    };
+  },
+
+  componentDidMount: function() {
+    $(ListStore).on('change', function () {
+      this.setState({
+        todoLists: ListStore.lists()
+      });
+    }.bind(this));
+    ListStore.getLists();
+  },
+
   render: function () {
+    var todoLists = [];
+    this.state.todoLists.forEach(function (todo) {
+      todoLists.push(<TodoList todo={todo} />);
+    });
     return (
       <div>
         <TodoListCreate />
-        <TodoList />
-
+        {todoLists}
       </div>
     );
   }
